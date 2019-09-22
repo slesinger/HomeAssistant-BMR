f9e92c9537798ea55f85b315c69058cd4ff8b571
Honza Slesinger <slesinger@gmail.com>
2019-09-22 20:58:28 +0200

    draft to group circuits to channels, darft set temp

--- custom_components/bmr/climate.py
+++ custom_components/bmr/climate.py
@@ -49,6 +49,22 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     user = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
     sensor_name = config.get(CONF_NAME)
+    channels = [
+        { "name": "Hala",      "floor": 0, "room": 14 },
+        { "name": "Loznice",   "floor": 1, "room": 15 },
+        { "name": "Technicka", "floor": 2, "room": 16 },
+        { "name": "Chodba",    "floor": 3, "room": 17 },
+        { "name": "Jidelna",   "floor": 4, "room": 18 },
+        { "name": "Obyvak",    "floor": 5, "room": 19 },
+        { "name": "Satna",     "floor": 6, "room": 20 },
+        { "name": "Kuba",      "floor": 7, "room": 21 },
+        { "name": "Chodba2",   "floor": 8, "room": 22 },
+        { "name": "Maja",      "floor": 9, "room": 23 },
+        { "name": "Koupelna",  "floor": 10},
+        { "name": "Koupelna2", "floor": 11},
+        { "name": "Zebrik1",   "room" : 12},
+        { "name": "Zebrik2",   "room" : 13}
+        ]
 
     bmr = pybmr.Bmr(host, user, password)
     cnt = bmr.getNumCircuits()
@@ -56,20 +72,27 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         raise Exception("Cannot connect to BMR")
 
     devices = []
-    for circuit_id in range(cnt):
-        device = Bmr(bmr, circuit_id)
-        devices.append(device)
+    if channels == None:
+        for circuit_id in range(cnt):
+            device = Bmr(bmr, circuit_id)
+            devices.append(device)
+    else:
+        for channel in channels:
+            device = Bmr(bmr, channel.get('room'), channel.get('floor'), channel['name'])
+            devices.append(device)
     add_devices(devices)
 
 
 class Bmr(ClimateDevice):
 
-    def __init__(self, bmr, circuit_id):
+    def __init__(self, bmr, circuit_id, floor_circuit_id=None, channel_name=None):
         import pybmr
         self._circuit_id = circuit_id
+        self._floor_circuit_id = floor_circuit_id
         self._bmr = bmr
         self._warning = None
         self._name = None
+        self._channel_name = channel_name
         self._current_temperature = None
         self._target_temperature = None
         self._max_allowed_floor_temperature = None
@@ -78,8 +101,8 @@ class Bmr(ClimateDevice):
         self._current_hvac_action = None
         self._unit = "Status"
         self._icon = "mdi:alert-decagram"
-
         self.update()
+
 
     @property
     def should_poll(self):
@@ -103,7 +126,10 @@ class Bmr(ClimateDevice):
 
     @property
     def name(self):
-        return '{}'.format(self._name)
+        if self._channel_name != None:
+            return '{}'.format(self._channel_name)
+        else:
+            return '{}'.format(self._name)
 
     @property
     def device_state_attributes(self):
@@ -157,24 +183,26 @@ class Bmr(ClimateDevice):
         self.manualUpdate()
 
     def manualUpdate(self):
-        status = self._bmr.getStatus(self._circuit_id)
-        if(status != None):
-            self._name = status['name']
-            if status['summer'] == 0 and status['cooling'] == 0 and status['heating'] == 0:
-                self._current_hvac_action = CURRENT_HVAC_IDLE
-            if status['summer'] == 0 and status['cooling'] == 1 and status['heating'] == 0:
-                self._current_hvac_action = CURRENT_HVAC_COOL
-            if status['summer'] == 1 and status['heating'] == 0:
-                self._current_hvac_action = CURRENT_HVAC_OFF
-            if status['heating'] == 1:
-                self._current_hvac_action = CURRENT_HVAC_HEAT
-            #self._? = status['enabled']
-            self._current_temperature = status['current_temp']
-            self._target_temperature = status['required_temp']
-            self._max_allowed_floor_temperature = None
-            self._current_floor_temperature = None
-            self._warning = status['warning']
-        else:
+        was_filled = False
+        self._warning = None
+        if self._floor_circuit_id != None:
+            floor_status = self._bmr.getStatus(self._floor_circuit_id)
+            if(floor_status != None):
+                self.manualUpdateFill(floor_status, 'floor')
+                was_filled = True
+
+        if self._circuit_id != None:
+            status = self._bmr.getStatus(self._circuit_id)
+            if(status != None):
+                self.manualUpdateFill(status, 'room')
+                was_filled = True
+        else: # Ensure required attributes are filled
+            if self._current_temperature == None:
+                self._current_temperature = floor_status['current_temp']
+            if self._target_temperature == None:
+                self._target_temperature = floor_status['required_temp']
+
+        if was_filled == False:
             self._warning = BMR_WARN_CANNOTCONNECT
             self._current_temperature = None
             self._target_temperature = None
@@ -183,28 +211,45 @@ class Bmr(ClimateDevice):
             self._current_hvac_mode = None
             self._current_hvac_action = None
 
-    def turn_on(self):
-        #self._current_hvac_mode = HVAC_MODE_ON
-        #self.bmr.setMode(2)
-        #self.bmr.exec()
-        self.manualUpdate()
 
-    def turn_off(self):
+    # It is assumed that for channels having both floor and room circuits the floor will get filled first and will get overwritten by room eventually
+    def manualUpdateFill(self, status, type):
+        self._name = status['name']
+        if status['summer'] == 0 and status['cooling'] == 0 and status['heating'] == 0:
+            self._current_hvac_action = CURRENT_HVAC_IDLE
+        if status['summer'] == 0 and status['cooling'] == 1 and status['heating'] == 0:
+            self._current_hvac_action = CURRENT_HVAC_COOL
+        if status['summer'] == 1 and status['heating'] == 0:
+            self._current_hvac_action = CURRENT_HVAC_OFF
+        if status['heating'] == 1:
+            self._current_hvac_action = CURRENT_HVAC_HEAT
+        #self._? = status['enabled']
+        if type == 'room':
+            self._current_temperature = status['current_temp']
+            self._target_temperature = status['required_temp']
+        if type == 'floor':
+            self._current_floor_temperature = status['current_temp']
+            self._max_allowed_floor_temperature = status['required_temp']
+        self._warning = status['warning'] #TODO room warning overwrites floor warning, needs condition here
+
+
+    def set_hvac_mode(self, hvac_mode):
         #self._current_hvac_mode = HVAC_MODE_OFF
         #self.bmr.setMode(0)
-        #self.bmr.exec()
         self.manualUpdate()
 
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
+        id = self._circuit_id
+        if id == None:
+            id = self._floor_circuit_id
         temperature = kwargs.get(ATTR_TEMPERATURE)
         if temperature is None:
             return
         elif(temperature >= 10 and temperature<= 27):
-            #self.bmr.setTemperature(temperature)
-            #self.bmr.exec()
+            self.bmr.setTemperature(temperature, id)
             self.manualUpdate()
         else:
             _LOGGER.warn("Chosen temperature=%s is incorrect. It needs to be between 10 and 27.", str(temperature))
-        
-            
+
+
